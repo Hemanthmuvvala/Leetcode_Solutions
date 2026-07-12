3898ab65c2ca84da945feafa18f5c06177cc7df7
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-12 23:15:23 +0530

    Create 1972-RotatingTheBox

---
+++ 1972-RotatingTheBox/1972-RotatingTheBox.java
@@ -0,0 +1,25 @@
+// Last updated: 7/12/2026, 11:15:23 PM
+class Solution {
+    public char[][] rotateTheBox(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        char[][] res = new char[cols][rows];
+        
+        for (int r = 0; r < cols; r++)
+            for (int c = 0; c < rows; c++)
+                res[r][c] = '.';
+
+        for (int r = 0; r < rows; r++) {
+            int p = cols - 1;
+            for (int c = cols - 1; c >= 0; c--) {
+                if (grid[r][c] == '*') {
+                    res[c][rows - 1 - r] = '*';
+                    p = c - 1;
+                } else if (grid[r][c] == '#')
+                    res[p--][rows - 1 - r] = '#';
+            }
+        }
+
+        return res;
+    }
+}
