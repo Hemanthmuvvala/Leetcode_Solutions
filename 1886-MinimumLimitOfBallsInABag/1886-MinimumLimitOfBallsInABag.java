06c581d9d6867d8dffb3663db2bda0da82e987b5
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-12 23:15:39 +0530

    Create 1886-MinimumLimitOfBallsInABag

---
+++ 1886-MinimumLimitOfBallsInABag/1886-MinimumLimitOfBallsInABag.java
@@ -0,0 +1,16 @@
+// Last updated: 7/12/2026, 11:15:39 PM
+class Solution {
+        public int minimumSize(int[] A, int k) {
+        int left = 1, right = 1_000_000_000;
+        while (left < right) {
+            int mid = (left + right) / 2, count = 0;
+            for (int a : A)
+                count += (a - 1) / mid;
+            if (count > k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
