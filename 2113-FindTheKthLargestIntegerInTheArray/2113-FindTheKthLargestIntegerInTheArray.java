7814f2cca09891b2f407aca757c6a60ec01e8952
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-12 23:15:05 +0530

    Create 2113-FindTheKthLargestIntegerInTheArray

---
+++ 2113-FindTheKthLargestIntegerInTheArray/2113-FindTheKthLargestIntegerInTheArray.java
@@ -0,0 +1,21 @@
+// Last updated: 7/12/2026, 11:15:05 PM
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+        PriorityQueue<String> pq=new PriorityQueue<>(
+            (a,b)->{
+                if(a.length()!=b.length())
+                    return a.length()-b.length();
+                return a.compareTo(b);
+            }
+        );
+
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
