134a26fea44820d5861e1e51e4e39351d2e7751b
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-13 00:11:24 +0530

    Create 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray

---
+++ 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.java
@@ -0,0 +1,12 @@
+// Last updated: 7/13/2026, 12:11:23 AM
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int operations = target[0];
+        for (int i = 1; i < target.length; i++) {
+            if (target[i] > target[i - 1]) {
+                operations += target[i] - target[i - 1];
+            }
+        }
+        return operations;
+    }
+}
