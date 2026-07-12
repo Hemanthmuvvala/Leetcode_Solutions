d5069ab14ab95aa918a1e16a76ef3248f36b1f20
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-13 00:11:36 +0530

    Create 1552-BuildAnArrayWithStackOperations

---
+++ 1552-BuildAnArrayWithStackOperations/1552-BuildAnArrayWithStackOperations.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2026, 12:11:36 AM
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans = new ArrayList<>();
+
+        int index = 0;
+
+        for (int i = 1; i <= n && index < target.length; i++) {
+
+            ans.add("Push");
+
+            if (i == target[index]) {
+                index++;
+            } else {
+                ans.add("Pop");
+            }
+        }
+
+        return ans;
+    }
+}
