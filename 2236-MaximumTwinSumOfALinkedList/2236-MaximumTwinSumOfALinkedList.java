8e966aab482bbec12e1e0f9ec9f31d8d521bf3dc
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-12 23:14:54 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ 2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.java
@@ -0,0 +1,32 @@
+// Last updated: 7/12/2026, 11:14:54 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        Stack<Integer> half = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            half.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        int res = 0;
+        while (slow != null) {
+            res = Math.max(res, half.pop() + slow.val);
+            slow = slow.next;
+        }
+
+        return res;
+    }
+}
