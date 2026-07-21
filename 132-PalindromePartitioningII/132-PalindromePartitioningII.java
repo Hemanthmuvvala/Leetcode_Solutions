362e060d7a2a54efad98a0dee8f6e336ffb7173c
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-21 23:21:45 +0530

    File updated at 7/21/2026, 11:21:44 PM

--- 132-PalindromePartitioningII/132-PalindromePartitioningII.java
+++ 132-PalindromePartitioningII/132-PalindromePartitioningII.java
@@ -1,40 +1,54 @@
-// Last updated: 7/21/2026, 10:04:20 PM
+// Last updated: 7/21/2026, 11:21:44 PM
 1class Solution {
 2    public int minCut(String s) {
-3        int n=s.length();
-4        int dp[]=new int[n];
+3        int n = s.length();
+4        int dp[] = new int[n];
 5
-6        Arrays.fill(dp,-1);
-7
-8        return getMin(0,dp,s)-1;
-9    }
+6        Arrays.fill(dp, -1);
+7        boolean pal[][]=new boolean[n][n];
+8        for (int i = n - 1; i >= 0; i--) {
+9            for (int j = i; j < n; j++) {
 10
-11    public static int getMin(int index,int dp[],String s){
-12
-13        if(index==s.length())return 0;
-14
-15        if(dp[index]!=-1) return dp[index];
-16        int min=Integer.MAX_VALUE;
-17        int count=0;
-18        for(int i=index;i<s.length();i++){
+11                if (s.charAt(i) == s.charAt(j)) {
+12                    if (j - i <= 2)
+13                        pal[i][j] = true;
+14                    else
+15                        pal[i][j] = pal[i + 1][j - 1];
+16                }
+17            }
+18        }
 19
-20            if(isPalindrome(index,i,s)){
-21                 count=1+getMin(i+1,dp,s);
-22            }
-23            min=Math.min(min,count);
-24        }
-25
-26        return dp[index]=min;   
-27    }
-28    public static boolean isPalindrome(int i,int j,String s){
-29        
-30       while(i<=j){
-31
-32        if(s.charAt(i)!=s.charAt(j)) return false;
+20        return getMin(0, dp, s,pal) - 1;
+21    }
+22
+23    public static int getMin(int index, int dp[], String s,boolean pal[][]) {
+24
+25        if (index == s.length())
+26            return 0;
+27
+28        if (dp[index] != -1)
+29            return dp[index];
+30        int min = Integer.MAX_VALUE;
+31        int count = 0;
+32        for (int i = index; i < s.length(); i++) {
 33
-34        i++;
-35        j--;
-36       }
-37        return true;
-38    }
-39}
+34            if (pal[index][i]) {
+35                count = 1 + getMin(i + 1, dp, s,pal);
+36            }
+37            min = Math.min(min, count);
+38        }
+39
+40        return dp[index] = min;
+41    }
+42    // public static boolean isPalindrome(int i,int j,String s){
+43
+44    //    while(i<=j){
+45
+46    //     if(s.charAt(i)!=s.charAt(j)) return false;
+47
+48    //     i++;
+49    //     j--;
+50    //    }
+51    //     return true;
+52    // }
+53}
