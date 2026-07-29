e675c18a7d4e07d8f1791cf98f272ed7c36946c2
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-07-29 14:29:25 +0530

    File updated at 7/29/2026, 2:29:25 PM

--- 940-DistinctSubsequencesII/940-DistinctSubsequencesII.java
+++ 940-DistinctSubsequencesII/940-DistinctSubsequencesII.java
@@ -1,14 +1,24 @@
-// Last updated: 7/28/2026, 11:21:29 PM
-1import java.util.*;
-2
-3class Solution {
-4
-5    public int distinctSubseqII(String S) {
-6        long end[] = new long[26], mod = (long) 1e9 + 7;
-7        for (char c : S.toCharArray())
-8            end[c - 'a'] = Arrays.stream(end).sum() % mod + 1;
-9        return (int) (Arrays.stream(end).sum() % mod);
-10    }
-11
-12    
-13}
+// Last updated: 7/29/2026, 2:29:25 PM
+1class Solution {
+2    static int MOD=1_000_000_007;
+3    public int distinctSubseqII(String S) {
+4       int n=S.length();
+5
+6       int dp[]=new int[n+1];
+7
+8       dp[0]=1;
+9       int arr[]=new int[26];
+10        Arrays.fill(arr,-1);
+11       for(int i=1;i<=n;i++){
+12            int c=S.charAt(i-1)-'a';
+13
+14            dp[i]=dp[i-1]*2%MOD;
+15            if(arr[c]!=-1){
+16                dp[i] = (dp[i] - dp[arr[c] - 1] + MOD) % MOD;
+17            }
+18            arr[c]=i;
+19       }
+20       return (dp[n] - 1 + MOD) % MOD;
+21    }
+22
+23}
