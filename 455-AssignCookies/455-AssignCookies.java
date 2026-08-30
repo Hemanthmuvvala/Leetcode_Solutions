cadd5eedc7350391424f137c138a1d5a517d0cda
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2026-08-30 21:37:18 +0530

    File updated at 8/30/2026, 9:37:17 PM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,26 +1,29 @@
-// Last updated: 8/30/2026, 9:35:59 PM
+// Last updated: 8/30/2026, 9:37:17 PM
 1class Solution {
 2    public int findContentChildren(int[] g, int[] s) {
-3        int n = g.length;
-4        int m = s.length;
-5
-6        Arrays.sort(g);
-7        Arrays.sort(s);
-8
-9        int count = 0;
-10
-11        int i = 0, j = 0;
-12        
-13        while(i < n &&  j < m) {
-14            if(g[i] <= s[j]) {
-15                count++;
-16                i++;
-17                j++;
-18            } else {
-19                j++;
-20            }
-21        }
-22
-23        return count;
-24    }
-25} 
+3        int i=0,j=0;
+4        int n=g.length;
+5        int m=s.length;
+6        int count=0;
+7        Arrays.sort(g);
+8        Arrays.sort(s);
+9        // for(int i=0;i<n;i++){
+10        //     if(i<m){
+11        //         for(int j=i;j<m;j++){
+12        //             if(s[j]>=g[i]){
+13        //                 count++;
+14        //                 break;
+15        //             }
+16        //         }
+17        //     }
+18        // }
+19        while(i<n && j<m){
+20            if(s[j]>=g[i]){
+21                count++;
+22                i++;
+23            }
+24            j++;
+25        }
+26        return count;
+27    }
+28}
